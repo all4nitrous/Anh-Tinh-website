0f2b6ceeb48825141468391ef71aab4a6bf63cf9
Nicholas Richmond <114439546+all4nitrous@users.noreply.github.com>
2022-12-13 23:33:54 -0600

    Done with header section

--- main.js
+++ main.js
@@ -2,3 +2,32 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 })
+
+
+//contact button (circular text)
+
+const  textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
