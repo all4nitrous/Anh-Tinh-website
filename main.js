f6ecf2dc702def5cc15388f75bdd0e8d14814f15
Nicholas Richmond <114439546+all4nitrous@users.noreply.github.com>
2022-12-22 01:12:29 -0600

    Issues with nav menu

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
 
@@ -31,14 +31,36 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+const nav = document.querySelector('.nav_links');
+const openNavBtn = document.querySelector('#nav_toggle-open');
+const closeNavBtn = document.querySelector('#nav_toggle-close');
+
+const openNav = () => {
+  nav.style.display = 'flex';
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+}
 
 
 
 
+openNavBtn.addEventListener('click', openNav);
+
+
+const closeNav = () => {
+  nav.style.display = 'none';
+  openNavBtn.style.display = 'inline-block';
+  closeNavBtn.style.display = 'none';
+}
 
 
 
 
+closeNavBtn.addEventListener('click', closeNav);
+
+nav.querySelectorAll('li a').forEach(navLink => {
+  navLink.addEventListener('click', closeNav);
+})
 
 
 
